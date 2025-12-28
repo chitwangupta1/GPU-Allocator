1645dadb9e7b82938cfca603ef3a200b468f834b
chitwangupta1 <154975998+chitwangupta1@users.noreply.github.com>
2025-12-28 21:30:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -682,4 +682,6 @@ if __name__ == '__main__':
     print("\nStarting server on http://localhost:5000")
     print("="*60 + "\n")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
