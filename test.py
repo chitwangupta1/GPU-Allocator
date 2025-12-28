88f962f3d84ee581386422ee18ce423d71f6be73
chitwangupta1 <154975998+chitwangupta1@users.noreply.github.com>
2025-12-28 20:40:28 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-API_KEY = os.getenv("GOOGLE_API_KEY")
-
-url = f"https://generativelanguage.googleapis.com/v1beta/models?key={API_KEY}"
-res = requests.get(url)
-
-print(res.json())
